03185fd6c5c60fdb409df783cc65fd1cf0329140
soheila <soheilalotfi@ymail.com>
2019-08-21 11:49:48 -0400

    creat the route

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-/// Routes
+// Routes
+
+app.get("/:operation/:firtNum/:secondNum", function(re, res) {
+  var operation = req.params.operation;
+  var firtNum = req.params.firtNum;
+  var secondNum = req.params.secondNum;
+});
 
 // start the server
 
